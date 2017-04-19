49bd09c4640d848536fe5b5c6f87532c0ac8e7e4
Rudy Wahjudi <rudy.wahjudi@gmail.com>
2017-04-19 12:40:50 +0700

    r1: login and logout works

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,26 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  let user = undefined;
+  res.render('index', { title: 'Bro', user: user, message: "" });
 });
+
+
+function checkSignIn(req, res, next){
+    if(req.session.user){
+        next();     //If session exists, proceed to page
+    } else {
+        var err = new Error("Not logged in!");
+        console.log(req.session.user);
+        next(err);  //Error, trying to access unauthorized page!
+    }
+}
+
+
+router.get('/home', checkSignIn, (req, res, next) => {
+  res.render('home', {title: "Home", user: req.session.user})
+})
+
+
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,54 @@
 var express = require('express');
 var router = express.Router();
+const db = require('../models');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('/new', (req, res, next) => {
+  res.render('new', {title: "Register"});
+})
+
+router.post('/new', (req, res, next) => {
+  var username = req.body.username;
+  var email = req.body.email;
+  var password = req.body.password;
+  var role = req.body.role;
+
+  db.User.create({username: username, email: email, password: password, role: role})
+  .then ( user => {
+    console.log(`created user ${user.username}.`);
+    res.render('index', {title: "Bro", user: user, message: ""});
+  })
+})
+
+router.post('/login', (req, res, next) => {
+
+  db.User.findOne({where: {username: req.body.username, password: req.body.password}})
+  .then( user => {
+
+    if (user) {
+      console.log(`find user ${user.username}`);
+      //set session
+      req.session.user = user;
+      res.redirect('/home');
+    }
+    else {
+      // res.redirect('/');
+      res.render('index', { title: 'Bro', user: undefined, message: 'Invalid Username or Password.' });
+    }
+
+  })
+})
+
+router.get('/logout', (req, res, next) => {
+  req.session.destroy( () => {
+    console.log("user logged out.")
+  })
+  res.redirect('/');
+})
+
 
 module.exports = router;
